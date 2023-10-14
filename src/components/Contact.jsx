c2e7932d86c7a76d1b2e8850d7b9862faf45631b
Joseph Dorman <JDorman555@yahoo.com>
2023-10-13 20:18:37 -0500

    setup form elements for contact info

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,25 @@ function Contact() {
 
   return (
     <>
-      Contact PLACEHOLDER
+      <form className='contact'>
+        <h2 className="contact-title">Contact Info</h2>
+        <div className="input-element">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" />
+        </div>
+        <div className="input-element">
+          <label htmlFor="tel">Phone Number</label>
+          <input type="tel" id="tel" name="tel" />
+        </div>
+        <div className="input-element">
+          <label htmlFor="website">Website</label>
+          <input type="url" id="url" name="url" />
+        </div>
+        <div className="input-element">
+          <label htmlFor="location">Location</label>
+          <input type="text" id="location" name="location" />
+        </div>
+      </form>
     </>
   )
 }
