70f4cdf4d869e2e26d8193baaeea5d53db4174bf
Joseph Dorman <JDorman555@yahoo.com>
2023-10-15 18:20:08 -0500

    add inputs for personal info

---
+++ src/components/Personal.jsx
@@ -0,0 +1,26 @@
+import '../styles/Personal.css'
+
+function Personal() {
+
+  return (
+    <>
+      <form className='personal'>
+        <h2 className="personal-title">Contact Info</h2>
+        <div className="input-element">
+          <label htmlFor="fname">First Name</label>
+          <input type="text" id="fname" name="fname" />
+        </div>
+        <div className="input-element">
+          <label htmlFor="lname">Last Name</label>
+          <input type="lname" id="lname" name="lname" />
+        </div>
+        <div className="input-element">
+          <label htmlFor="title">Title</label>
+          <input type="text" id="title" name="title" />
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default Personal
