12dbb702dbcbe3d460e0d6c12f19899a162ee63c
Joseph Dorman <JDorman555@yahoo.com>
2023-10-15 20:05:40 -0500

    create rough draft of elements

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import '../styles/Header.css'
+
+function Header() {
+  return (
+    <>
+      <header>
+        <div className="head">
+          <h1 className="header-title">CV Generator</h1>
+          <div className="buttons"></div>
+        </div>
+        <p className="desc"></p>
+        <p className="link"></p>
+      </header>
+    </>
+  )
+}
+
+export default Header
