5ccbed78624cd974f8cbcb6d81b362e2ac0c2c8f
Joseph Dorman <JDorman555@yahoo.com>
2023-10-17 20:18:15 -0500

    update className

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -6,9 +6,9 @@ import download from '/download.svg'
 function Buttons() {
   return (
     <>
-      <button className="reset"><img className="icon" src={reset}></img></button>
-      <button className="autofill"><img className="icon" src={autofill}></img></button>
-      <button className="download"><img className="icon" src={download}></img></button>
+      <button className="reset"><img className="m-icon" src={reset}></img></button>
+      <button className="autofill"><img className="m-icon" src={autofill}></img></button>
+      <button className="download"><img className="m-icon" src={download}></img></button>
     </>
   )
 }
