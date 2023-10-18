a9d5efe50cf0ec41cf4020cc67edd781c62c8d02
Joseph Dorman <JDorman555@yahoo.com>
2023-10-17 21:24:41 -0500

    finish component and it's styling

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -4,7 +4,31 @@ import education from '/education.svg'
 function Education() {
   return (
     <>
-
+      <form className='education'>
+        <h2 className="form-title"><img className='l-icon' src={education}></img>Education</h2>
+        <div className="input-element">
+          <label htmlFor="school">School</label>
+          <input type="text" id="school" name="school" />
+        </div>
+        <div className="input-element">
+          <label htmlFor="degree">Degree</label>
+          <input type="text" id="degree" name="degree" />
+        </div>
+        <div className="timeline">
+          <div className="input-element">
+            <label htmlFor="start-date">Start Date</label>
+            <input type="date" id="start-date" name="start-date" />
+          </div>
+          <div className="input-element">
+            <label htmlFor="end-date">End Date</label>
+            <input type="date" id="end-date" name="end-date" />
+          </div>
+          <div className="input-element">
+            <label htmlFor="present">On Going?</label>
+            <input type="checkbox" id="present" name="present" />
+          </div>
+        </div>
+      </form>
     </>
   )
 }
