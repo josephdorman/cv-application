d2bf3c78bc5106c7c66f3c192f01976782cb02d9
Joseph Dorman <JDorman555@yahoo.com>
2023-10-18 23:12:13 -0500

    setup resume header

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -5,7 +5,16 @@ function Resume() {
   return (
     <>
       <div className="resume">
-        Resume PLACEHOLDER
+        <div className="resume-header">
+          <h1 className="resume-name">Joseph Dorman</h1>
+          <h3 className='resume-position'>Web Developer</h3>
+          <div className="resume-contacts">
+            <p className="resume-phone">908-761-0478</p>
+            <p className='resume-website'>https://github.com/josephdorman</p>
+            <p className="resume-email">jdorman@profession.mail</p>
+            <p className="resume-location">Los Angeles, CA</p>
+          </div>
+        </div>
       </div>
     </>
   )
