0bf8a3fdc9f96b724e0d394fa1c469564a81c584
Joseph Dorman <JDorman555@yahoo.com>
2023-10-18 23:33:52 -0500

    finish resume header in component

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,4 +1,8 @@
 import '../styles/Resume.css'
+import phone from '/phone.svg'
+import website from '/website.svg'
+import email from '/email.svg'
+import location from '/location.svg'
 
 function Resume() {
 
@@ -8,11 +12,11 @@ function Resume() {
         <div className="resume-header">
           <h1 className="resume-name">Joseph Dorman</h1>
           <h3 className='resume-position'>Web Developer</h3>
-          <div className="resume-contacts">
-            <p className="resume-phone">908-761-0478</p>
-            <p className='resume-website'>https://github.com/josephdorman</p>
-            <p className="resume-email">jdorman@profession.mail</p>
-            <p className="resume-location">Los Angeles, CA</p>
+          <div className="resume-contact-container">
+            <p className="resume-contact"><img src={phone} className="s-icon" />908-761-0478</p>
+            <p className='resume-contact'><img src={website} className="s-icon" />github.com/josephdorman</p>
+            <p className="resume-contact"><img src={email} className="s-icon" />jdorman@profession.mail</p>
+            <p className="resume-contact"><img src={location} className="s-icon" />Los Angeles, CA</p>
           </div>
         </div>
       </div>
