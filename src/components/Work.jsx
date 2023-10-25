d9e59a5187b6eee739bc7a6e5ad60f4f0e1374db
Joseph Dorman <JDorman555@yahoo.com>
2023-10-24 23:33:51 -0500

    add an add button for work form

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,7 +5,7 @@ function Work() {
   return (
     <>
       <form className='work'>
-        <h2 className="form-title"><img className='l-icon' src={work}></img>Work</h2>
+        <h2 className="form-header"><span className="form-title"><img className='l-icon' src={work}></img>Work</span><span className='button-container'><button className='add'>Add</button></span></h2>
         <div className="input-element">
           <label htmlFor="company">Company Name</label>
           <input type="text" id="company" name="company" />
