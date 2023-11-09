2f332ff81f6469426f68cb5aeefb7502d04cdcb0
Joseph Dorman <JDorman555@yahoo.com>
2023-11-08 22:34:20 -0600

    finish func to reset all of form

--- src/components/Reset.jsx
+++ src/components/Reset.jsx
@@ -1,3 +1,59 @@
-export default function Reset () {
-  
+import { v4 as uuidv4 } from 'uuid'
+
+export default function Reset (data, setData) {
+  const info = [
+    {
+      name: 'fname',
+      text: ''
+    },
+    {
+      name: 'lname',
+      text: ''
+    },
+    {
+      name: 'title',
+      text: ''
+    },
+    {
+      name: 'tel',
+      text: ''
+    },
+    {
+      name: 'url',
+      text: ''
+    },
+    {
+      name: 'email',
+      text: ''
+    },
+    {
+      name: 'location',
+      text: ''
+    }
+  ]
+
+  const eduInfo = [
+    { id: uuidv4(),
+      school: "",
+      degree: "",
+      location: '',
+      start: "",
+      end: "",
+      present: ""}
+  ]
+
+  const expInfo = [
+    {
+      id: uuidv4(),
+      company: '',
+      position: '',
+      locationWork: '',
+      startWork: '',
+      endWork: '',
+      presentWork: '',
+      workDescription: ''
+    }
+  ]
+
+  setData({info: info, education: eduInfo, experience: expInfo});
 }
