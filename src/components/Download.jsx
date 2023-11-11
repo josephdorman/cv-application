a82a6d50d1481f0e3569b3abf8df15178a54e2cb
Joseph Dorman <JDorman555@yahoo.com>
2023-11-10 20:59:46 -0600

    import jspdf/create func to download resume as pdf

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,3 +1,16 @@
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
+
 export default function Download () {
-  
+  const html = document.querySelector('.resume');
+
+  html2canvas(html)
+    .then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+
+      const pdf = new jsPDF();
+      pdf.addImage(imgData, 'PNG', 0, 0);
+      pdf.save("download.pdf");  
+  })
+
 }
