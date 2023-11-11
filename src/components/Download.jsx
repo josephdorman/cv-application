86c661c7f81313f1eaacbe3395caf13ba093e49f
Joseph Dorman <JDorman555@yahoo.com>
2023-11-10 21:52:34 -0600

    change pdf size and name

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -8,9 +8,14 @@ export default function Download () {
     .then((canvas) => {
       const imgData = canvas.toDataURL('image/png');
 
-      const pdf = new jsPDF();
+      const pdf = new jsPDF({
+        orientation: "portrait",
+        unit: "px",
+        format: [478, 640]
+      });
+
       pdf.addImage(imgData, 'PNG', 0, 0);
-      pdf.save("download.pdf");  
+      pdf.save("application.pdf");  
   })
 
 }
