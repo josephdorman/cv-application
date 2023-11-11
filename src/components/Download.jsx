ab9fed2fd6d6d7ea5202e51cef3375e583482d05
Joseph Dorman <JDorman555@yahoo.com>
2023-11-10 22:57:30 -0600

    change to dynamically get size of required space for pdf

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -8,14 +8,12 @@ export default function Download () {
     .then((canvas) => {
       const imgData = canvas.toDataURL('image/png');
 
-      const pdf = new jsPDF({
-        orientation: "portrait",
-        unit: "px",
-        format: [478, 640]
-      });
+      var pdf = new jsPDF('p', 'pt', [canvas.width, canvas.height]);
+      var pdfWidth = pdf.internal.pageSize.getWidth();
+      var pdfHeight = pdf.internal.pageSize.getHeight();
+      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+      pdf.save("mypdf.pdf");
 
-      pdf.addImage(imgData, 'PNG', 0, 0);
-      pdf.save("application.pdf");  
   })
 
 }
